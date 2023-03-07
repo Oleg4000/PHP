c2bd092be525a2579027b5fb45791168e64998a7
Oleg4000 <olegklim568@gmail.com>
2023-03-07 21:17:25 +0200

    anonimus function and sort

--- Lesson 40/index.php
+++ Lesson 40/index.php
@@ -30,6 +30,57 @@
     }
 
     echo outter(), inner();
+    echo "<br>";
+
+    //динамічне ім'я ф-ції
+    function first()
+    {
+        return "First function";
+    }
+    function second()
+    {
+        return "Second function";
+    }
+    $newFunction = rand(0, 1) ? 'first' : 'second';
+    echo $newFunction;
+    echo "<br>";
+
+    //anonimus function
+    /* $arr = ['PHP', 'JS', 'C++', 'Python'];
+    sort($arr);
+    echo '<pre>';
+    print_r($arr);
+    echo '<pre>';
+ */
+    class Point
+    {
+        public $x;
+        public $y;
+    }
+    $fst = new Point;
+    $fst->x = 12;
+    $fst->y = 5;
+
+    $snd = new Point;
+    $snd->x = 1;
+    $snd->y = 1;
+
+    $thd = new Point;
+    $thd->x = 4;
+    $thd->y = 10;
+
+    $arr = [$fst, $snd, $thd];
+
+    usort($arr, function ($a, $b) {
+        $dist_a = sqrt($a->x ** 2 + $a->y ** 2);
+        $dist_b = sqrt($b->x ** 2 + $b->y ** 2);
+        return $dist_a <=> $dist_b;
+    });
+
+
+    echo '<pre>';
+    print_r($arr);
+    echo '<pre>';
     ?>
 </body>
 
