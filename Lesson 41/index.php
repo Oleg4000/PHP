1e4a06bc039df2ba4c1b583a8a40e865353c3852
Oleg4000 <olegklim568@gmail.com>
2023-03-12 13:58:29 +0200

    Использование нескалярных типов в качестве значений по умолчанию

--- Lesson 41/index.php
+++ Lesson 41/index.php
@@ -21,7 +21,7 @@
     echo '<br>';
     echo $message; */
 
-    function odd(int $number)
+    /*     function odd(int $number)
     {
         if ($number % 2 == 0) {
             return false;
@@ -29,7 +29,41 @@
             return true;
         }
     }
-    echo odd(4);
+    echo odd(4); */
+    //підрахунок к-ті аргументів і повернення результату
+
+    function sum(...$items)
+    {
+        $sum = 0;
+        for ($i = 0; $i < count($items); $i++) {
+            $sum += $items[$i];
+        }
+        return $sum;
+    }
+    echo sum(10, 5, 15);
+    echo '<br>';
+    $a = [5, 15, 25, 35, 15, 45];
+    echo '<br>';
+
+    //func_num_args()-Возвращает количество аргументов, переданных текущей пользовательской функции.
+    function foo()
+    {
+        echo "Количество аргументов: ", func_num_args(), PHP_EOL;
+    }
+
+    echo foo(1, 2, 3, 4, 5, 6, 7, 8, 85, 19, 25, 65, 551541, 26511);
+
+
+
+    echo '<br>';
+    //Использование нескалярных типов в качестве значений по умолчанию
+    function makecoffee($types = array("капучино"), $coffeeMaker = NULL)
+    {
+        $device = is_null($coffeeMaker) ? "вручную" : $coffeeMaker;
+        return "Готовлю чашку " . join(", ", $types) . " $device.\n";
+    }
+    echo makecoffee();
+    echo makecoffee(array("капучино", "лавацца"), "в чайнике");
     ?>
 </body>
 
