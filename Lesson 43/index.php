dbe5f1e1e3a46316accb868e3d6f47f06404e94c
Oleg4000 <olegklim568@gmail.com>
2023-03-20 20:53:05 +0200

    str

--- Lesson 43/index.php
+++ Lesson 43/index.php
@@ -10,7 +10,32 @@
 
 <body>
     <?php
+    /* $date = '01.01.2021';
+    echo 'day' . substr($date, 0, 2) . '<br>';
+    echo 'mount' . substr($date, 3, 2) . '<br>';
+    echo 'year' . substr($date, 6) . '<br>'; */
+    /* day01-mount01-year2021 */
 
+    //search
+    /* $date = 'Hello world';
+    echo strpos($date, 'world'); */
+    //search 2
+    /*  $str = 'PHP-інтерпретована мова';
+    echo substr($str, strpos($str, 'інтерпрет')); */
+
+    //підміна
+    /*  $str = 'PHP-[b]інтерпретована[/b] мова. [b]та жирний текст[/b]';
+    echo $str, '<br>';
+    $str = str_replace(['[b]', '[/b]'], ['', ''], $str, $number);
+
+    echo "Здійснено заміну " . $number . " рази"; */
+
+
+    //delete
+    $str = ' Hello, world!  ';
+    echo strlen($str);
+    echo '<br>';
+    echo strlen(trim($str));
     ?>
 </body>
 
